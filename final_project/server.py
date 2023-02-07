e9374b13152fe809417a405ec193c32ef50cc3bc
Nethsara Siyum <nethsara@apium.io>
2023-02-08 02:28:57 +0530

    updated server.py

--- final_project/server.py
+++ final_project/server.py
@@ -7,13 +7,13 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    french_text = translator.translate(textToTranslate, "en", "fr")
+    french_text = translator.translate_english_to_french(textToTranslate)
     return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    english_text = translator.translate(textToTranslate, "fr", "en")
+    english_text = translator.translate_french_to_english(textToTranslate)
     return english_text
 
 @app.route("/")
